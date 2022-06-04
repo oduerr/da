78dc07c95ac58b12a863b9486f855531d5b7056b
Oliver4242 <oliver.duerr@gmail.com>
2022-06-04 08:51:00 +0200

    Missing bits and pieces

--- cheat_sheet/Cheat_Sheet.R
+++ cheat_sheet/Cheat_Sheet.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(dplyr)
 library(magrittr)
+
+############ Log and Exp ##############
 xs  = seq(-3,3,0.01) 
 xss = seq(0.01,3,0.01)
 d = data.frame(x = xss, vals=log(xss));d$f='log';
@@ -12,8 +14,9 @@ rbind(d, dd) %>%
   ylab(latex2exp::TeX(r'($f(x)$)')) +
   coord_cartesian(xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5)) +
   theme_linedraw(base_size = 22) +
-  theme(legend.position = c(0.2,0.80)) + 
+  theme(legend.position = c(0.2,0.82)) + 
   theme(legend.title = element_blank()) +
   guides(color=guide_legend(override.aes=list(fill=NA)))
   
 ggsave('exp_log.svg', width = 5, height = 5)
+ggsave('exp_log.pdf', width = 5, height = 5)
---
+++ data/Munich_Airbnb.R
@@ -0,0 +1,3 @@
+#Data from munich Airbnb from David Rügammer
+# https://github.com/davidruegamer/airbnb
+dat = readRDS("~/Downloads/airbnb-main/munich_clean.RDS")
