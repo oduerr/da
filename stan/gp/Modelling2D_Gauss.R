2c7c8b2e34c5ea6139e45cbf28e2a4bd1e64d124
Oliver4242 <oliver.duerr@gmail.com>
2022-06-19 13:11:56 +0200

    Added description of Cholesky Decomposition in notebook

--- stan/gp/Modelling2D_Gauss.R
+++ stan/gp/Modelling2D_Gauss.R
@@ -4,10 +4,10 @@ library(manipulate)
 manipulate({
   rho = matrix(c(1,cor,cor,1), nrow=2)
   sigma = matrix(c(s1,0,0,s2), nrow=2)
-  Sigma = sigma %*% rho %*% sigma
+  K = sigma %*% rho %*% sigma
   
   #Sampling of data from MV-Gaussian
-  d = data.frame(mvrnorm(n = N, Sigma = Sigma, mu=c(mu1,mu2)))
+  d = data.frame(mvrnorm(n = N, Sigma = K, mu=c(mu1,mu2)))
   #Drawing of the plot
   gg = ggplot(d, aes(x=X1, y=X2)) +
     geom_point(size=0.5, alpha=0.5) +
@@ -19,8 +19,8 @@ manipulate({
 },
 s1=slider(0,5,1, step = 0.1),
 s2=slider(0,5,1, step = 0.1),
+cor=slider(-1,1,0, step = 0.1),
 mu1=slider(-5,5,0, step = 0.5),
-mu2=slider(-5,5,0, step = 0.5),
-cor=slider(-1,1,0, step = 0.1)
+mu2=slider(-5,5,0, step = 0.5)
 )
 
