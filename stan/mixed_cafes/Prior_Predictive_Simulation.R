c8ef6d645734b13f9c2c49226416e28c2645e521
Oliver Dürr <oliver.duerr@gmail.com>
2023-06-07 11:36:22 +0200

    Update Prior_Predictive_Simulation.R

--- stan/mixed_cafes/Prior_Predictive_Simulation.R
+++ stan/mixed_cafes/Prior_Predictive_Simulation.R
@@ -1,12 +1,13 @@
 N = 1e5
 mu = runif(N,1,20)
 sigma= rcauchy(N, 3, 2)
+#We remove points far away 
 ok = sigma > 0 & sigma < 100
 sum(ok)
 mu = mu[ok]
 sigma = sigma[ok]
 
 
-hist(mu, xlim = c(0,20))
-hist(sigma,1000, freq = FALSE, xlim=c(0,20))
-hist(rnorm(sum(ok),mu, sigma), 1000,xlim=c(0,20), freq = FALSE)
+hist(mu, xlim = c(0,20), xlab='mu', freq = FALSE)
+hist(sigma,1000, xlab='sigma', xlim=c(0,20), freq = FALSE)
+hist(rnorm(sum(ok),mu, sigma), xlab='y', 1000,xlim=c(0,20), freq = FALSE)
