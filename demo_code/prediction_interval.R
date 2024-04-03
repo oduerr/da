bc11b7d06cc13704d7a2fba9edaf19bcadb1af5b
Oliver Dürr <oliver.duerr@gmail.com>
2024-04-03 07:33:47 +0200

    Update prediction_interval.R

--- demo_code/prediction_interval.R
+++ demo_code/prediction_interval.R
@@ -3,6 +3,7 @@ n=length(xs)
 set.seed(2)
 ys = rnorm(n, mean=xs * 1 + 0.5, sd=1) 
 plot(xs,ys, xlim=c(-6,6), ylim=c(-10,10), xlab='x',ylab='mu|x', main='Confidence Intervalls for mu', sub = '200 random betas')
+#plot(xs,ys, xlim=c(-6,6), ylim=c(-10,10), xlab='x',ylab='')
 l = lm(ys ~ xs)
 abline(l, col='green', lwd=2)
 d = summary(l)
@@ -15,3 +16,12 @@ for (i in 1:100){
   abline(l, col='green', lwd=2)
   points(xs,ys, xlim=c(-4,4), ylim=c(-10,10))
 }
+
+
+datanew <- data.frame(xs = seq(-6,6,by = 0.1))
+pred1 <- predict(l, newdata = datanew, interval = "confidence", level=0.9) 
+pred2 <- predict(l, newdata = datanew, interval = "prediction", level=0.9)
+lines(datanew$xs, pred1[,2], col = "blue") 
+lines(datanew$xs, pred1[,3], col = "blue") 
+lines(datanew$xs, pred2[,2], col = "red") 
+lines(datanew$xs, pred2[,3], col = "red")
