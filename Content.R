2d7be233f9c00062355982f4efeee5ccd235aaac
Oliver Dürr <oliver.duerr@gmail.com>
2024-06-06 09:37:35 +0200

    Transformation of Likelihood

---
+++ Content.R
@@ -0,0 +1,18 @@
+# Internal links
+
+### Docu
+## Stan Primer
+"/Users/oli/Documents/GitHub/da/website/Stan_Primer_Full.qmd"
+"/Users/oli/Documents/GitHub/da/website/about.qmd" # Some note how to render the website
+"/Users/oli/Documents/GitHub/da/website/lecture_notes/MCMC/MCMC.qmd"
+
+
+### Arbeitsblätter
+
+"/Users/oli/Library/CloudStorage/Dropbox/__HTWG/DataAnalytics/_Current/lab/06_03_Bayes_3/bayes_3_compile.R"
+
+#### Bayes Woche 4 ###
+
+"/Users/oli/Documents/GitHub/da/stan/logreg_challenger/logistic_regression.R" # Logistic Regression for slides
+"/Users/oli/Library/CloudStorage/Dropbox/__HTWG/DataAnalytics/_Current/lab/06_04_Bayes_4/bayes_4_compile.R"
+"/Users/oli/Dropbox/__HTWG/DataAnalytics/_Current/lab/06_04_Bayes_4/bayes_corona_adapted.qmd"
